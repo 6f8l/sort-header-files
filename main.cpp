74e08f2ed2cdf6a9d7d08865d38f5ea3f7cdc8d4
6f8l <atsuki02jojo@gmail.com>
2020-08-08 16:40:20 +0900

    Add: methods to process I/O header files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+class Header_File_Line {
+protected:
+  string line;
+
+public:
+  void setLine(const string &newLine) { line = newLine; }
+  string getLine() { return line; }
+};
 
 class File {
 protected:
@@ -11,16 +22,50 @@ protected:
 public:
   void setName(const string &newName) { name = newName; }
   string getName() { return name; }
-  void outputResult() {}
-};
 
-class Header_File {
-protected:
-  string name;
+  // outputSortedFiles creates "output.txt" that contains sorted header files.
+  void outputSortedFiles() {
+    vector<Header_File_Line> hf_lines;
+    ofstream outfile("output.txt", ios_base::out);
 
-public:
-  void setName(const string &newName) { name = newName; }
-  string getName() { return name; }
+    hf_lines = getSortedHeaderFiles();
+
+    for (vector<Header_File_Line>::iterator itr = hf_lines.begin();
+         itr != hf_lines.end(); ++itr) {
+      outfile << itr->getLine() << "\n";
+    }
+
+    outfile.close();
+    cout << "Sorted header files are shown in \"output.txt\"" << endl;
+  }
+
+  // sortHeaderFiles sorts the vector elements by rules.
+  vector<Header_File_Line> sortHeaderFiles(vector<Header_File_Line> lines) {
+    // TODO
+    return lines;
+  }
+
+  // getSortedHeaderFiles returns vector that contains sorted header files.
+  vector<Header_File_Line> getSortedHeaderFiles() {
+    vector<Header_File_Line> hf_lines;
+    string line;
+    ifstream datafile(name);
+
+    while (getline(datafile, line)) {
+      Header_File_Line hf_line;
+
+      // Add header files that contain "#include" in the line to header_files
+      if (!line.find("#include")) {
+        hf_line.setLine(line);
+        hf_lines.push_back(hf_line);
+      }
+    }
+
+    // Execute sort.
+    hf_lines = sortHeaderFiles(hf_lines);
+
+    return hf_lines;
+  }
 };
 
 int main(int argc, char *argv[]) {
@@ -37,6 +82,6 @@ int main(int argc, char *argv[]) {
     file.setName(argv[1]);
   }
 
-  cout << file.getName() << endl;
+  file.outputSortedFiles();
   return 0;
 }
